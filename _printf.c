3dca0173287d304fea0a63f377c77780457d00a3
Pendarium <thomas.sorli21@gmail.com>
2025-07-22 17:15:39 +0200

    updt

--- _printf.c
+++ _printf.c
@@ -55,6 +55,6 @@ int _printf(const char *format, ...)
 
 va_end(args);
 
-return(0);
+return(count);
 }
 
--- main.c
+++ main.c
@@ -11,11 +11,12 @@ int main(void)
 {
     int len;
     int len2;
-    unsigned int ui;
-    void *addr;
 
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
+
+        printf("%d %d\n", len,len2); 
+    /*
     ui = (unsigned int)INT_MAX + 1024;
     addr = (void *)0x7ffe637541f0;
     _printf("Length:[%d, %i]\n", len, len);
@@ -39,6 +40,6 @@ int main(void)
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
     _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");*/
     return (0);
 }
