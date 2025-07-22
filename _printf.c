7e15095159a9f946baf24437b42082bb5ca5b58e
Pendarium <thomas.sorli21@gmail.com>
2025-07-22 16:13:16 +0200

    updt

--- _printf.c
+++ _printf.c
@@ -13,6 +13,7 @@ int _printf(const char *format, ...)
 {
 
 	int i = 0;
+	int count = 0;
 
 	va_list args;
 	va_start (args, format);
@@ -20,27 +21,31 @@ int _printf(const char *format, ...)
 	while (format[i] != '\0') /*detection du texte*/
 	{
 			_putchar (format[i]);
+			count++;
 			i++;
 		
 			if (format[i] == '%' && format[i + 1] == '%')/*cas du %*/
 			{
 				_putchar('%');
+				count++;
 				i += 2;
-				continue;
+
 			}
 			else if (format[i] == '%' && format[i + 1] == 'c')/*cas du int*/
 			{
 				char result = va_arg(args, int);
 				_putchar(result);
+				count++;
 				i += 2;
-				continue;
+
 			}
 			else if (format[i] == '%' && format[i + 1] == 's')/*cas du str*/
 			{
 				char *result = va_arg(args, char*);
 				_puts(result);
+				count++;
 				i += 2;
-				continue;
+
 			}
 	}
 va_end(args);
