def52870dea43fd4febecdb0342c8de303d4293d
Pendarium <thomas.sorli21@gmail.com>
2025-07-22 14:39:15 +0200

    try to succes task 1

--- _printf.c
+++ _printf.c
@@ -2,43 +2,45 @@
 
 int _printf(const char *format, ...)
 {
-	size_t i = 0;
+	int compteur = 0;
+	int i = 0;
 
 	va_list args;
 	va_start (args, format);
 
 	while (format[i] != '\0')
 	{
-		if (format[i] == '%')
+		if (format[i] == '%' && format[i + 1] == '%')			
 		{
-			if (format[i] + 1 == '%')
-			{
-				_putchar('%');
-				i++;
-				continue;
-			}
-			else if (format[i] + 1 == 'c')
-			{
-				char result = va_arg(args, int);
-				_putchar(result);
-				i++;
-				continue;
-			}
-			else if (format[i] + 1 == 's')
-			{
-				char *result = va_arg(args, char*);
-				_puts(result);
-				i++;
-				continue;
-			}
+			_putchar('%');
+			compteur++;
+			i += 2;
+			continue;
+		}
+		else if (format[i] == '%' && format[i + 1] == 'c')
+		{
+			char result = va_arg(args, int);
+			_putchar(result);
+			compteur++;
+			i += 2;
+			continue;
+		}
+		else if (format[i] == '%' && format[i + 1] == 's')
+		{
+			char *result = va_arg(args, char*);
+			_puts(result);
+			compteur++;
+			i += 2;
+			continue;
 		}
 		else
 		{
 			_putchar (format[i]);
+			compteur++;
 			i++;
 		}
-	}
-	va_end(args);
+}
+va_end(args);
 	
 return(0);
 }
