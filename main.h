9a4fd47606fb8c0317b8aabdf71cfe5b043bcc42
CecileLAROCHE <11458@holbertonstudents.com>
2025-07-22 18:06:51 +0200

    ecriture des cas %i et %d -

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int _strlen(char *s);
+void _puts(char *str);
+void print_numbers(void);
+
+#endif
