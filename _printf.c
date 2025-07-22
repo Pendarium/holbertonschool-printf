44614ccf381767530e43201540566dae4a075930
Pendarium <thomas.sorli21@gmail.com>
2025-07-22 12:07:00 +0200

    first try to execute function printf with condition

--- _printf.c
+++ _printf.c
@@ -2,14 +2,43 @@
 
 int _printf(const char *format, ...)
 {
-	int i;
+	size_t i = 0;
+
 	va_list args;
 	va_start (args, format);
 
-	for (size_t i = 0; format[i] != '\0'; i++)
+	while (format[i] != '\0')
 	{
-		
+		if (format[i] == '%')
+		{
+			if (format[i] + 1 == '%')
+			{
+				_putchar('%');
+				i++;
+				continue;
+			}
+			else if (format[i] + 1 == 'c')
+			{
+				char result = va_arg(args, int);
+				_putchar(result);
+				i++;
+				continue;
+			}
+			else if (format[i] + 1 == 's')
+			{
+				char *result = va_arg(args, char*);
+				_puts(result);
+				i++;
+				continue;
+			}
+		}
+		else
+		{
+			_putchar (format[i]);
+			i++;
+		}
 	}
+	va_end(args);
 	
-	
+return(0);
 }
