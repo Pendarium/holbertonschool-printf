23b388a681267c72ab8f7545c1e7bf3771765814
CecileLAROCHE <11458@holbertonstudents.com>
2025-07-23 10:26:37 +0200

    essai de debeugage de la tache 1

--- _printf_t1.c
+++ _printf_t1.c
@@ -3,18 +3,6 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-    return (write(1, &c, 1));
-}
-
-/**
  * _printf - Custom printf function
  * @format: The format string
  *
@@ -24,46 +12,45 @@ int _putchar(char c)
  */
 int _printf(const char *format, ...)
 {
-    int i = 0;
-    int count = 0;
-    va_list args;
+	int i = 0;
+	int count = 0;
+	va_list args;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    /* Vérifie si la chaîne est NULL */
-    if (format == NULL)
-    {
-        return (-1);
-    }
+	/* Vérifie si la chaîne est NULL */
+	if (format == NULL)
+	{
+		return (-1);
+	}
 
-    while (format[i] != '\0') /* Détection du texte */
-    {
-        if (format[i] == '%')  /* Si on trouve un % */
-        {
-            if (format[i + 1] == 'd' || format[i + 1] == 'i') 
-            {
-                /* Cas du d ou i - Entier décimal signé */
-                count += print_numbers(va_arg(args));
-                i += 2;  /* Avance après % et d/i */
-            }
-            else
-            {
-                /* Affiche % si ce n'est pas suivi d'un format valide */
-                _putchar(format[i]);
-                count++;  /* Comptabilise le % */
-                i++;  /* Avance d'un caractère */
-            }
-        }
-        else
-        {
-            /* Affiche un caractère normal */
-            _putchar(format[i]);
-            count++;  /* Comptabilise le caractère normal */
-            i++;  /* Avance d'un caractère */
-        }
-    }
-
-    va_end(args);
-
-    return (count); /* Retourne le nombre total de caractères imprimés */
+	while (format[i] != '\0') /* Détection du texte */
+	{
+		if (format[i] == '%')  /* Si on trouve un % */
+		{
+			if (format[i + 1] == 'd' || format[i + 1] == 'i')
+			{
+				/* Cas du d ou i - Entier décimal signé */
+				count += print_numbers(va_arg(args, int));
+				i += 2;  /* Avance après % et d/i */
+				return (void);
+			}
+			else
+			{
+				/* Affiche % si ce n'est pas suivi d'un format valide */
+				_putchar(format[i]);
+				count++;  /* Comptabilise le % */
+				i++;  /* Avance d'un caractère */
+			}
+		}
+		else
+		{
+			/* Affiche un caractère normal */
+			_putchar(format[i]);
+			count++;  /* Comptabilise le caractère normal */
+			i++;  /* Avance d'un caractère */
+		}
+	}
+	va_end(args);
+	return (count); /* Retourne le nombre total de caractères imprimés */
 }
