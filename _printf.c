83ddd0e661425f4ef4659c7051e3b0bd1d8b18e0
CecileLAROCHE <11458@holbertonstudents.com>
2025-07-23 14:38:03 +0200

    mise en forme pour betty et reduction du nombre de ligne même si c'est moche ;)

--- _printf.c
+++ _printf.c
@@ -1,68 +1,44 @@
 #include "main.h"
 
-
+/**
+*/
 
 int _printf(const char *format, ...)
 {
-    int i = 0, count = 0;
-    va_list args;
+	int i = 0, count = 0, j = 0;
+	va_list args;
 
-    va_start(args, format);
-
-while (format[i] != '\0')
+	va_start(args, format);
+	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
 			if (format[i + 1] == '%')
-			{
-				_putchar('%');
+			{_putchar('%');
 				count++;
-				i += 2;
-			}
-
-            else if (format[i + 1] == 'c')
-            {
-                char ch = va_arg(args, int);
-                _putchar(ch);
-                count++;
-                i += 2;
-            }
-            else if (format[i + 1] == 's')
-            {
-                char *str = va_arg(args, char *);
-                int j = 0;
-
-                if (str == NULL)
-                    str = "(null)";
-
-                while (str[j])
-
-                {
-                    _putchar(str[j]);
-                    count++;
-                    j++;
-                }
-                i += 2;
-            }
-            else
-            {
-
-                _putchar(format[i]);
-                _putchar(format[i + 1]);
-                count += 2;
-                i += 2;
-            }
-        }
-        else
-        {
-            _putchar(format[i]);
-            count++;
-            i++;
-        }
-    }
-
-    va_end(args);
-
-    return count;
-}
-
+				i += 2; }
+			else if (format[i + 1] == 'c')
+			{char ch = va_arg(args, int);
+				_putchar(ch);
+				count++;
+				i += 2; }
+			else if (format[i + 1] == 's')
+			{char *str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(null)";
+				while (str[j])
+				{_putchar(str[j]);
+					count++;
+					j++; }
+				i += 2; }
+			else
+			{_putchar(format[i]);
+				_putchar(format[i + 1]);
+				count += 2;
+				i += 2; } }
+		else
+		{_putchar(format[i]);
+			count++;
+			i++; } }
+	va_end(args);
+	return (count); }
