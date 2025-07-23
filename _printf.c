0b8267d37e9004e13e6ff32afc62969f8e3ed259
Pendarium <thomas.sorli21@gmail.com>
2025-07-23 10:47:27 +0200

    conflict

--- _printf.c
+++ _printf.c
@@ -16,39 +16,41 @@ int _printf(const char *format, ...)
 	int count = 0;
 
 	va_list args;
+
 	va_start (args, format);
 
-	while (format[i] != '\0') /*detection du texte*/
+	while (format[i] != '\0' && format[i] != '%') /*detection du texte*/
 	{
-		if (format[i] != '%')
-			{
 			_putchar (format[i]);
 			count++;
 			i++;
-			}
-
+			
 			if (format[i] == '%' && format[i + 1] == '%')/*cas du %*/
 			{
 				_putchar('%');
 				count++;
 				i += 2;
 
+
 			}
 			else if (format[i] == '%' && format[i + 1] == 'c')/*cas du int*/
 			{
 				char result = va_arg(args, int);
+
 				_putchar(result);
 				count++;
 				i += 2;
+
 
 			}
 			else if (format[i] == '%' && format[i + 1] == 's')/*cas du str*/
 			{
 				char *result = va_arg(args, char*);
+
 				_puts(result);
 				count++;
 				i += 2;
-
+				
 			}
 
 	}
