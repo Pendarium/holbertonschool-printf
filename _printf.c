14df2f827efa73334a914efc49919a7a0707185f
CecileLAROCHE <11458@holbertonstudents.com>
2025-07-23 11:37:22 +0200

    betty

--- _printf.c
+++ _printf.c
@@ -14,14 +14,13 @@ int _printf(const char *format, ...)
 
 	int i = 0;
 	int count = 0;
-
 	va_list args;
 
 	va_start (args, format);
 
 	while (format[i] != '\0' && format[i] != '%') /*detection du texte*/
 	{
-			_putchar (format[i]);
+		_putchar (format[i]);
 			count++;
 			i++;
 			
@@ -60,4 +59,3 @@ va_end(args);
 
 return(i);
 }
-
